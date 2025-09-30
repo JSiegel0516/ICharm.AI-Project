24d5304abb6ee95d1e90c70987908daf077b6541
William Phong <williamphong@users.noreply.github.com>
2025-09-30 12:55:38 -0700

    icons files

---
+++ src/app/(frontpage)/_components/SideSettings.tsx
@@ -0,0 +1,11 @@
+import { SettingsIcon } from '@/app/(frontpage)/_components/SettingsIcon';
+
+const SideSettings = () => {
+  return (
+    <div className="">
+      <SettingsIcon size={24} />
+    </div>
+  );
+};
+
+export default SideSettings;
