c98764178c60bc15a6f5722212e1cd9422a98f62
William Phong <williamphong@users.noreply.github.com>
2025-10-19 21:05:32 -0700

    betterauth implementation with drizzle+postgres

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,34 @@
 import { betterAuth } from "better-auth";
-import { Pool } from "pg";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from 'better-auth/next-js';
+import { cache } from 'react';
+import { headers } from 'next/headers';
+import { db } from "@/lib/db/index"; // your drizzle instance
 
 export const auth = betterAuth({
-  database: new Pool({
-        // connection options
+    database: drizzleAdapter(db, {
+        provider: "pg", 
     }),
+    emailAndPassword: { 
+    enabled: true, 
+  }, 
+plugins: [nextCookies()],
+  socialProviders: {
+        google: { 
+            prompt: "select_account consent", 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+            accessType: "offline", 
+        }, 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    },
+});
+
+export const getSession = cache(async () => {
+  return await auth.api.getSession({
+    headers: await headers(),
+  });
 });
